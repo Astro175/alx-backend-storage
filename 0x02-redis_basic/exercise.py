a4c3bc1d5a6e5774b6b27edcfff9977fd55aba44
astro175 <aspark400@gmail.com>
2023-08-17 05:05:33 +0100

    added task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Creates a class, generates a key with uuid
+   stores data inside the key.
+"""
+import redis
+import uuid
+
+class Cache:
+    """Class that stores data in a redis key"""
+    def __init__(self):
+        """Creates a redis instance"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: str | bytes | int | float) -> str:
+        """method that Creates a key and store data"""
+        key = str(uuid.uuid1())
+        self_redis.mset({key: data})
+        return key
+
+
