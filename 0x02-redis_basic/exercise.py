863993168c147fef58f993fa81a6ce4a3626452b
astro175 <aspark400@gmail.com>
2023-08-17 05:53:19 +0100

    added task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,6 +4,8 @@
 """
 import redis
 import uuid
+from typing import Union, Callable, Optional
+
 
 class Cache:
     """Class that stores data in a redis key"""
@@ -18,4 +20,36 @@ class Cache:
         self._redis.mset({key: data})
         return key
 
+    def get(self, key: str,
+            fn: Optional[callable] = None) -> Union[str, bytes,
+                                                    int, float]:
+        """
+        get method that take a key string argument and an
+        optional Callable argument named fn. This callable
+        will be used to convert the data back to the desired format.
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(key)
+        return value
 
+    def get_str(self, key: str) -> str:
+        """
+          This will be used to convert the data back to the string.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return data.decode('utf-8')
+
+    def get_int(self, key):
+        """
+          This will be used to convert the data back to int.
+        """
+
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return int(data.decode("utf-8"))
