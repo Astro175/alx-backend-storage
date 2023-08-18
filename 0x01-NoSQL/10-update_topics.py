ca1a5b8871c0c9d15013fbc5d206f42a2fb34281
astro175 <aspark400@gmail.com>
2023-08-18 17:39:24 +0100

    added task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+  A Python function that changes all topics of a school
+  document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+      a Python function that changes all topics of a
+      school document based on the name
+    """
+
+    result = mongo_collection.update_many(
+            {"name": name}, {'$set': {"topics": topics}})
+    return result
